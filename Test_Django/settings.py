d24cff034c60f25f0479174d4d02af081473e446
elmasarany <a.elmasarany@gmail.com>
2019-05-31 21:42:32 +0200

    try CI

--- Test_Django/settings.py
+++ Test_Django/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hello',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
